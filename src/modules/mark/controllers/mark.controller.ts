308901dc1d03853da0afa7bab6807dfe51b6df51
REINALDO <andersonnreinaldo@gmail.com>
2025-01-08 23:45:18 -0300

    marcas

--- src/modules/mark/controllers/mark.controller.ts
+++ src/modules/mark/controllers/mark.controller.ts
@@ -1,6 +1,6 @@
 import { Controller, Get, Post, Param, Body, Put, UseGuards } from '@nestjs/common';
 import { MarkService } from '../services/mark.service';
-import { marca } from '@prisma/client';
+import { marcas } from '@prisma/client';
 import { AuthGuard } from 'src/modules/auth/auth.guard';
 
 @Controller('mark')
@@ -9,25 +9,25 @@ export class MarkController {
 
   @UseGuards(AuthGuard)
   @Get()
-  findAll(): Promise<marca[]> {
+  findAll(): Promise<marcas[]> {
     return this.markService.findAll();
   }
 
   @UseGuards(AuthGuard)
   @Get(':id')
-  findOne(@Param('id') id: number): Promise<marca> {
+  findOne(@Param('id') id: number): Promise<marcas> {
     return this.markService.findOne(+id);
   }
 
   @UseGuards(AuthGuard)
   @Post()
-  create(@Body() stock: Omit<marca, 'id' | 'createdAt' | 'updatedAt'>): Promise<marca> {
+  create(@Body() stock: Omit<marcas, 'id' | 'createdAt' | 'updatedAt'>): Promise<marcas> {
     return this.markService.create(stock);
   }
   
   @UseGuards(AuthGuard)
   @Put(':id')
-  update(@Param('id') id: number, @Body() stock: Partial<marca>): Promise<marca> {
+  update(@Param('id') id: number, @Body() stock: Partial<marcas>): Promise<marcas> {
     return this.markService.update(+id, stock);
   }
 }
